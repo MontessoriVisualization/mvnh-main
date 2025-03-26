f8666c3e67d97a839e142f4c1c5863af5c06925f
MontessoriVisualization <MontessoriVisualization@gmailcom>
2025-03-26 12:38:19 +0545

    Update code to deploy production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+// filepath: c:\Users\LENOVO\Pictures\MVNH-main\vite.config.ts
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MVNH",
+  base: "/MVNH/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    chunkSizeWarningLimit: 1000, // e.g., increase the limit to 1000 kB
+    chunkSizeWarningLimit: 1000,
   },
-})
+});
