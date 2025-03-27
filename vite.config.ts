5eb3e0ac1754b64c19ade4ecd7cf456730505c24
MontessoriVisualization <MontessoriVisualization@gmailcom>
2025-03-27 19:58:05 +0545

    pusg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MVNH/",
+  base: "/mvnh-main/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
